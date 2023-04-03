66d0bc7d3c7a98b619136546cda2b975f3a7fc1b
Gaidysheff <gaidysheff@mail.ru>
2023-04-03 22:30:07 +0500

    Django & React connected

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import axios from "axios";
+import React, { Component } from "react";
+
+class App extends Component {
+  state = { details: [] };
+
+  componentDidMount() {
+    let data;
+    axios
+      .get("http://localhost:8000")
+      .then((res) => {
+        data = res.data;
+        this.setState({ details: data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <header>Данные из DJANGO</header>
+        <hr /> <hr />
+        {this.state.details.map((output, id) => (
+          <div key={id}>
+            <div>
+              <h2>{output.title}</h2>
+              <p>{output.description}</p>
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  }
+}
+
+export default App;
