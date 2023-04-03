f2694f9e37e92681fe9465668a7248f3ac529d08
Gaidysheff <gaidysheff@mail.ru>
2023-04-03 19:59:01 +0500

    django application runs

--- recipes_django_react/backend_api/views.py
+++ recipes_django_react/backend_api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Recipe
+from .serializer import RecipeSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class RecipeView(APIView):
+    def get(self, request):
+        output = [
+            {
+                'title': output.title,
+                'description': output.description,
+            } for output in Recipe.objects.all()
+        ]
+        return Response(output)
+
+    def post(self, request):
+        serializer = RecipeSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
--- recipes_django_react/recipes_django_react/settings.py
+++ recipes_django_react/recipes_django_react/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backend_api',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +50,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'recipes_django_react.urls'
 
